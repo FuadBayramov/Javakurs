e8208ca4444e139ec9b8405fda8865914234f3ee
FuadBayramov <fuad.bayramli30@gmail.com>
2025-02-15 17:11:31 +0400

    Createproject

---
+++ codes/spring/springprojecct/springprojecct/src/main/java/az/developia/springprojecct/SpringprojecctApplication.java
@@ -0,0 +1,13 @@
+package az.developia.springprojecct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringprojecctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringprojecctApplication.class, args);
+	}
+
+}
---
+++ codes/spring/springprojecct/springprojecct/src/test/java/az/developia/springprojecct/SpringprojecctApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.springprojecct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringprojecctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
